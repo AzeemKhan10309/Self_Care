5b2a0fb034967015ab0154a5ca53decbc36b81be
Azeem khan <fqr.azzy786@gmail.com>
2025-08-11 16:50:35 +0500

    Add Health-Tracker-Screen

---
+++ Frontend/Screens/HealthTracker/Component/ProgressCard/ProgressCard.styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  card: { backgroundColor: '#1a73e8', borderRadius: 16, padding: 16, alignItems: 'center' },
+  header: { flexDirection: 'row', alignItems: 'center', marginBottom: 4 },
+  title: { color: '#fff', fontWeight: '600', marginRight: 246, fontSize: 16 },
+  subtitle: { color: '#cce0ff', fontSize: 13, marginBottom: 16,marginRight: 156 },
+  circle: { alignItems: 'center', marginBottom: 16 , borderRadius: 100, width: 100, height: 100, backgroundColor: '#3f51b5', justifyContent: 'center'},
+  steps: { fontSize: 32, color: '#fff', fontWeight: '700' },
+  stepsLabel: { fontSize: 14, color: '#cce0ff' },
+  infoRow: { flexDirection: 'row', justifyContent: 'space-around', width: '100%' },
+  infoItem: { alignItems: 'center' },
+  infoValue: { fontSize: 18, color: '#fff', fontWeight: '700' },
+  infoLabel: { fontSize: 12, color: '#cce0ff' },
+  info: { fontSize: 20, color: '#fff', marginBottom: 4 },
+});
+export default styles;
